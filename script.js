d3f24348ea8502e65d0c7f53e47e4b8c6bc76cbe
Michèle R <mraharinosy@gmail.com>
2020-04-04 21:49:03 +0200

    Création CV html + maj menu responsive design

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 // script JS
 $(function(){
+    $('.dropdown-toggle').dropdown()
 // lancemement du jeu
     $("#boutonStart").click(function(){
         $(this).hide(1000);
-        $("#taupeShow1").attr("src","assets/trouTaupe1.png")
-        $("#taupeShow2").attr("src","assets/trouTaupe1.png")
-        $("#taupeShow3").attr("src","assets/trouTaupe1.png")
-        $("#taupeShow4").attr("src","assets/trouTaupe1.png")
-        $("#taupeShow5").attr("src","assets/trouTaupe1.png")
-        $("#taupeShow6").attr("src","assets/trouTaupe1.png")
     });
 
 //clique sur une taupe sortie
