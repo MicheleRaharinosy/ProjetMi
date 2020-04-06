1ef2ae375af2de9c641c614c82e4d7d65d735134
Michèle R <mraharinosy@gmail.com>
2020-04-06 19:56:07 +0200

    isole script taupe

---
+++ script-trou.js
@@ -0,0 +1,17 @@
+// script JS
+$(function(){
+
+    // lancemement du jeu
+        $("#boutonStart").click(function(){
+            $(this).hide(1000);
+        });
+    
+        $("#trou_init img.anim").each( function (index) {
+            $(this).addClass("step"+(index+1));
+        });
+    
+        $(".trou").each(function () {
+            $(this).html($("#trou-init").html());
+        });
+    
+    });
--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// script JS
 $(function(){
     $('.dropdown-toggle').dropdown()
-// lancemement du jeu
-    $("#boutonStart").click(function(){
-        $(this).hide(1000);
-    });
-
-    $("#trou_init img.anim").each( function (index) {
-        $(this).addClass("step"+(index+1));
-    });
-
-    $(".trou").each(function () {
-        $(this).html($("#trou-init").html());
-    });
-    
-
-    
-
-
-
-
 });
