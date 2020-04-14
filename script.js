6970047569a7a85284176024108e9f7a79c306b5
Michèle R <mraharinosy@gmail.com>
2020-04-14 11:21:08 +0200

    debut chrono

--- script.js
+++ script.js
@@ -7,35 +7,48 @@ $(function(){
     console.log(imageTrouTaupe);
     var btnDemarrer = document.getElementById('boutonStart');
     
-    
-   
-    btnDemarrer.addEventListener('click',function(){alert('Go baby Go')},false);    
-    
-    btnDemarrer.addEventListener('click',apparitionTaupe,false); 
-    
+    btnDemarrer.addEventListener('click',function(){chrono()},false);    
+    btnDemarrer.addEventListener('click',function(){apparitionTaupe()},false);  
 
+    
+    function chrono(){
+        
+        var timerJeu = document.getElementById("timerJeu");
+        
+        var intervalTimer = setInterval(function(){
+            var timerZero = new Date();
+            var maintenant = timerZero.getSeconds();
+            timerJeu.innerHTML = maintenant
+            ,1000});      
+            setTimeout(function(){
+                clearInterval(intervalTimer)
+            },20000);  
+        
+    };
+    
     function apparitionTaupe (){
         var imageTaupe = document.createElement("img");            
         var imageTaupe2 = document.createElement("img");
         imageTaupe.src = "assets/trou-taupe-0.png";
         document.querySelector("#trou1").appendChild(imageTaupe);
         trou1.classList.toggle("visible");   
-
+        
         imageTaupe2.src = "assets/trou-taupe-3.png";
         document.querySelector("#trou2").appendChild(imageTaupe2);
         trou2.classList.toggle("visible");
         var interval = setInterval(function(){
             trou1.classList.toggle("visible");
-        },2000);
+        },1000);
         var interval2 = setInterval(function(){
             trou2.classList.toggle("visible");
         },4000);
-            setTimeout(function(){ 
-                clearInterval(interval),
-                clearInterval(interval2)
-            },20000);
+        setTimeout(function(){ 
+            clearInterval(interval),
+            clearInterval(interval2)
+        },20000);
     };    
-                  
+    
+ 
        
 
        
